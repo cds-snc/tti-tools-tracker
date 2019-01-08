3db04f40f9d595869d0dcd9f52ca32f9281afc90
Tim Arney <tim@line37.com>
2019-01-08 12:01:55 -0500

    Request lighthouse data

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 import octokit from "@octokit/rest";
 export { notify } from "./notify";
 export { validate } from "./validate";
-export { build } from "./build";
+export { requestTti } from "./requestTti";
 export { loadFromFirestore, saveToFirestore } from "./firestore";
 export default octokit();
