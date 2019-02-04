b124cbc0cca180c7c52b17e506521666df42ada3
Max <max@neuvians.net>
2019-02-04 14:51:37 -0500

    New mock

--- src/lib/notify.js
+++ src/lib/notify.js
@@ -37,8 +37,9 @@ export const notify = async (
     },
     status
   );
-
+  console.log(statusObj);
   const result = await client.repos.createStatus(statusObj);
+  console.log("RESULT", result);
 
   return result;
 };
